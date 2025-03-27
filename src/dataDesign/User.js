c7fa4a86a1090770121e7a0b4eadcbc2aaaa35c8
Keyu Lin <keyulin12345@gmail.com>
2025-03-26 20:54:13 -0400

    some updates for user.js

--- src/dataDesign/User.js
+++ src/dataDesign/User.js
@@ -5,41 +5,41 @@ class User {
   #major;
 
   constructor() {
-    this.user_id = '0000';
-    this.completed_courses = [];
-    this.interests = '';
-    this.major = 'None';
+    this.#user_id = '0000';
+    this.#completed_courses = [];
+    this.#interests = '';
+    this.#major = 'None';
   }
 
   constructor(user_id, completed_courses, interests, major) {
-    this.user_id = user_id;
-    this.completed_courses = completed_courses;
-    this.interests = interests;
-    this.major = major;
+    this.#user_id = user_id;
+    this.#completed_courses = completed_courses;
+    this.#interests = interests;
+    this.#major = major;
   }
   
   addCompletedCourse(course_name) {
-    this.completed_courses.push(course_name);
+    this.#completed_courses.push(course_name);
   }
 
   getCompletedCourses() {
-    return this.completed_courses;
+    return this.#completed_courses;
   }
   
   updateInterests(newInterests) {
-    this.interests = newInterests;
+    this.#interests = newInterests;
   }
 
   getInterests() {
-    return this.interests;
+    return this.#interests;
   }
 
   changeMajor(newMajor) {
-    this.major = newMajor;
+    this.#major = newMajor;
   }
 
   getMajor() {
-    return this.major;
+    return this.#major;
   }
 }
 export default User;
