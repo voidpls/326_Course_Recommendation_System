b01b689fc4d457c89a30e6d3a3765f7488d94d96
Void <voidpls@protonmail.com>
2025-04-10 00:24:24 -0400

    Compatibility with conversion to commonjs

--- src/Components/course_progress_chart/course_progress_chart.js
+++ src/Components/course_progress_chart/course_progress_chart.js
@@ -1,4 +1,4 @@
-export async function courseProgressPageSetup() {
+window.courseProgressPageSetup = async function() {
     const cs_bs_2023_flowchart = await fetch('Components/course_progress_chart/cs_bs_2023_flowchart.html');
     const html = await cs_bs_2023_flowchart.text();
     const parent = document.getElementById('course-flowchart-container');
