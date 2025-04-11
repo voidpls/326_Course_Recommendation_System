a334772f01a45e756d9f48b4828c6ad8bca24093
shreyBaha <bahadurshrey@gmail.com>
2025-04-11 19:33:11 -0400

    shows selected course

--- src/Components/course-review-page/course_review_page.js
+++ src/Components/course-review-page/course_review_page.js
@@ -13,6 +13,41 @@ async function loadCourses(){
         let course_item = document.createElement("li")
         course_item.className = "course-item"
         course_item.innerHTML = `<div class="rating-box"><h1 class="rating-text">3.1</h1></div><div class="name-box"><h2 class="name-text">${course.code}: ${course.title}</h2></div>`
+        //course_item.onclick = courseClick
+        course_item.addEventListener("click", function (e) {courseClick(course)}, false)
+        course_item.params = course
         courseListElement.appendChild(course_item)
     }
+}
+
+function courseClick(course){
+    changeSelected(course)
+    removeReviews()
+    loadReviews()
+}
+
+function changeSelected(course){
+    console.log(course)
+    let selectedTitle = document.getElementById("course-item-selected")
+    let prerequisites = document.getElementById("prerequisites")
+    let instructors = document.getElementById("instructors")
+    let credits = document.getElementById("credits")
+    selectedTitle.innerHTML = `
+        <div class="rating-box">
+            <h1 class="rating-text">3.1</h1>
+        </div>
+        <div class="name-box">
+            <h1 class="name-text">${course.code}: ${course.title}</h1>
+        </div>`
+    prerequisites.innerHTML = `Prerequisites: ${course.prerequisites}`
+    credits.innerHTML = `Credits: ${course.credits}`
+    instructors.innerHTML = `Instructors: ${course.instructors}`
+}
+
+function removeReviews(){
+    console.log('reviews removed')
+}
+
+function loadReviews(){
+    console.log('reviews loaded')
 }
