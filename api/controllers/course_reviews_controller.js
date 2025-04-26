c44be5745618b8925451fd9ff562a21f6fe8ca01
shreyBaha <bahadurshrey@gmail.com>
2025-04-25 22:21:34 -0400

    added endpoints and integrated

--- api/controllers/course_reviews_controller.js
+++ api/controllers/course_reviews_controller.js
@@ -1,8 +1,30 @@
+let reviewData = {fake: 'data'}
+
 exports.getReviews = async (req, res, next) => {
-    const { courseId } = req.params;
-    try {
-      res.json({ courseId, /* ...whatever data... */ });
+  const { courseId } = req.params;  
+  try {
+      res.json({courseId, reviewData});
     } catch (err) {
       next(err);
     }
+};
+
+exports.createReview = async (req, res, next) => {
+  try {
+      review = req.body; 
+      reviewData = {review}
+      res.status(201).json({ message: 'Review created successfully', data: review });
+  } catch (err) {
+      next(err);
+  }
+};
+
+exports.deleteReview = async (req, res, next) => {
+  const { reviewId } = req.params; 
+  try {
+      reviewData = {}; 
+      res.json({ message: 'Profile deleted successfully', id: reviewId});
+  } catch (err) {
+      next(err);
+  }
 };
